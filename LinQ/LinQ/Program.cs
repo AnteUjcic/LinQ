0f2a85227376bd4fd4bd9291d4b9e38d02cca105
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-11-15 08:52:36 +0100

    Add files via upload

---
+++ LinQ/LinQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+	public static void Main()
+	{
+		
+		IList<string> stringList = new List<string>() {
+			"hamburger",
+			"cheese Burger",
+			"cheese Burek",
+			"Piletina s cheese" ,
+			"Shrek"
+		};
+
+		
+		var result = from s in stringList
+					 where s.Contains("cheese")
+					 select s;
+
+		foreach (var str in result)
+		{
+			Console.WriteLine(str);
+		}
+	}
+}
+
---
+++ LinQMethod/LinQMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LinQMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			
+			IList<string> stringList = new List<string>() {
+			"Tosted cheese",
+			"CHEESE",
+			"shrek 101",
+			"how 2 be shrek" ,
+			"burger shrek"
+		};
+
+			
+			var result = stringList.Where(s => s.Contains("shrek"));
+
+
+			foreach (var str in result)
+			{
+				Console.WriteLine(str);
+			}
+		}
+    }
+}
